71d3dd6eaa15516270453795ab4ec531cbba890f
Bernardo Cezar <b.cezar1812@gmail.com>
2024-07-31 19:27:56 -0300

    created abstract method withdraw, created Bank_account class and methods deposit and text

---
+++ bank_accounts.py
@@ -0,0 +1,19 @@
+import abc
+
+
+class Bank_account(abc.ABC):
+    def __init__(self, agency, num_account, balance):
+        self.agency = agency
+        self.num_account = num_account
+        self.balance = balance
+
+    @abc.abstractmethod
+    def withdraw(self, value): ...
+
+    def deposit(self, value):
+        self.balance += value
+        self.text(f'Depositando...R$ {value}')
+
+    # Método para fim de teste quando ao andamento do projeto
+    def text(self, msg):
+        print(f'Saldo é {self.saldo:.2f} {msg}')
