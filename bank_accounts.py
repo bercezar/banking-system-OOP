9d05de7ec21534f6b4d751642da9510b886ecc0a
Bernardo Cezar <b.cezar1812@gmail.com>
2024-07-31 19:55:16 -0300

    created SavingsAccount class and applied withdraw method

--- bank_accounts.py
+++ bank_accounts.py
@@ -15,5 +15,23 @@ class BankAccount(abc.ABC):
         self.text(f'Depositando...R$ {value}')
 
     # Método para fim de teste quando ao andamento do projeto
-    def text(self, msg):
-        print(f'Saldo é {self.saldo:.2f} {msg}')
+    def test(self, msg):
+        print(f'Saldo é {self.balance:.2f} {msg}')
+
+
+class SavingsAccount(BankAccount):
+    def withdraw(self, value):
+
+        # Para a utilização do método withdraw(sacar), primeiro verifica-se se há balance(saldo) disponível. Será retornado o valor pós saque.
+        postWithdrawalAmount = self.balance - value
+
+        if postWithdrawalAmount >= 0:
+            self.balance -= value
+            self.test(f'Valor do saque é {value:.2f}')
+            return self.balance
+        # Caso entre na condição, não será executado por conta do return. Caso contrário, o print é executado.
+        print(f'Saque negado: R$ {value:.2f}. Confira o seu saldo em conta.')
+
+
+if __name__ == "__main__":
+    sa1 = SavingsAccount(100, 200, 300)  # TESTE
